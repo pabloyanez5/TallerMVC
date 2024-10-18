7d713f6d3abf474c88f011ea0fc9cc1147fb9f54
pabloyanez5 <130101518+pabloyanez5@users.noreply.github.com>
2024-10-18 09:03:51 -0500

    Upload 3
    Commit 3

--- Taller1PY/Program.cs
+++ Taller1PY/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Taller1PY.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Taller1PYContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Taller1PYContext") ?? throw new InvalidOperationException("Connection string 'Taller1PYContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
