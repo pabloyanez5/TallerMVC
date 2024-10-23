1e454479d8cc20a49f1b4908e5969753bc39f6a4
pabloyanez5 <130101518+pabloyanez5@users.noreply.github.com>
2024-10-23 06:18:53 -0500

    Commit 4

--- Taller1PY/Models/Equipo.cs
+++ Taller1PY/Models/Equipo.cs
@@ -1,11 +1,17 @@
-﻿namespace Taller1PY.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Taller1PY.Models
 {
-    public class Equipo
+    public class Jugador
     {
+        [Key]
         public int Id { get; set; }
         public string Nombre { get; set; }
-        public string Ciudad { get; set; }
-        public int Titulos { get; set; }
-        public string AceptaExtranjeros { get; set; }
+        public string Posicion { get; set; }  
+        public int Edad { get; set; }
+        [ForeignKey("Equipo")]
+        public Equipo Equipo { get; set; }
+
     }
 }
